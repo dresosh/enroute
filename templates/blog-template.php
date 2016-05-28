fa844332956f13bc41aede11c6e84323308bec10
dresosh <contact@dresosh.com>
2016-05-27 17:42:48 -0700

    adds padding to bottom of blogs, adds different arrows to homepage carousel, adds responsive styles for image captions

--- templates/blog-template.php
+++ templates/blog-template.php
@@ -12,6 +12,11 @@
     <div class="container">
       <div class="row">
         <div class="col-md-offset-2 col-md-8 post-container">
+
+          <?php $args = array( 'post_type' => 'blog', 'posts_per_page' => -1 ) ?>
+          <?php $loop = new WP_Query( $args ); ?>
+          <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
           <div class="single-post">
             <div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-icon-color="transparent">
               <a class="a2a_button_facebook"></a>
@@ -20,12 +25,16 @@
               <a class="a2a_button_sms"></a>
               <a class="a2a_button_whatsapp"></a>
             </div>
-            <h3>May 20</h3>
-            <h2>Blog Header</h2>
+            <h3><?php the_time( get_option( 'date_format' ) ); ?></h3>
+            <h2><?php the_title(); ?></h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              <?php the_content(); ?>
             </p>
           </div>
+
+          <?php endwhile; ?>
+
+
         </div>
       </div>
     </div>
--- templates/home-template.php
+++ templates/home-template.php
@@ -40,11 +40,13 @@
 
         <!-- Controls -->
         <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <i class="fa fa-chevron-circle-left" aria-hidden="true"></i>
+          <!-- <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> -->
           <span class="sr-only">Previous</span>
         </a>
         <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <i class="fa fa-chevron-circle-right" aria-hidden="true"></i>
+          <!-- <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> -->
           <span class="sr-only">Next</span>
         </a>
       </div>
